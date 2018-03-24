b398b1bfb77111acb2c31fff52e89f2217db2c0e
bucali <ayetullah_boztas16@erdogan.edu.tr>
2018-03-24 17:46:48 +0300

    Sitede gönderi oluşturmak ve düzenlemek için view'ler eklendi.

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,4 +5,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 ]
